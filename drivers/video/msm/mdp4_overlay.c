748d78450bbfd6a1d0cd6a6eca6401e278bf3af2
Mayank Chopra <makchopra@codeaurora.org>
2012-03-03 06:51:20 +0530

    [SQUASHED] CAF/CM-14.1 patch for interleaved YUV format to MDP.
    msm_fb: display: Add pseudo-planar 422 H1V2 support in the MDP4
    
    Rotator outputs pseudo-planar 422 H1V2 for interleaved inputs
    on 90 degree rotation. Add changes to support MDP_Y_CRCB_H1V2
    in MDP4.
    
    CRs-Fixed: 371303
    Change-Id: I730a3196df8d0c204e6334c2c8b9199d15cbb16a
    Signed-off-by: Mayank Chopra <makchopra@codeaurora.org>
    Signed-off-by: Padmanabhan Komanduru <pkomandu@codeaurora.org>
    
    msm: rotator: Add pseudo-planar 422 H1V2 dst format for MDP4
    
    Rotator outputs pseudo-planar 422 H1V2 for interleaved inputs
    on 90 degree rotation.Add changes to support MDP_Y_CRCB_H1V2,
    MDP_Y_CBCR_H1V2 in MDP4.
    
    CRs-Fixed: 371303
    Change-Id: I42ea86992fc77cd335437a47611e9f640f30ed25
    Signed-off-by: Mayank Goyal <goyalm@codeaurora.org>
    Signed-off-by: Mayank Chopra <makchopra@codeaurora.org>
    Signed-off-by: Padmanabhan Komanduru <pkomandu@codeaurora.org>
    
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- drivers/video/msm/mdp4_overlay.c
+++ drivers/video/msm/mdp4_overlay.c
@@ -863,6 +863,8 @@ static void mdp4_overlay_vg_get_src_offset(struct mdp4_overlay_pipe *pipe,
 
 		case MDP_Y_CRCB_H2V1:
 		case MDP_Y_CBCR_H2V1:
+		case MDP_Y_CRCB_H1V2:
+		case MDP_Y_CBCR_H1V2:
 			*luma_off = pipe->src_x +
 				(pipe->src_y * pipe->srcp0_ystride);
 			*chroma_off = pipe->src_x +
@@ -1071,6 +1073,8 @@ int mdp4_overlay_format2type(uint32 format)
 	case MDP_YCRYCB_H2V1:
 	case MDP_Y_CRCB_H2V1:
 	case MDP_Y_CBCR_H2V1:
+	case MDP_Y_CRCB_H1V2:
+	case MDP_Y_CBCR_H1V2:
 	case MDP_Y_CRCB_H2V2:
 	case MDP_Y_CBCR_H2V2:
 	case MDP_Y_CBCR_H2V2_TILE:
@@ -1254,6 +1258,8 @@ int mdp4_overlay_format2pipe(struct mdp4_overlay_pipe *pipe)
 		break;
 	case MDP_Y_CRCB_H2V1:
 	case MDP_Y_CBCR_H2V1:
+	case MDP_Y_CRCB_H1V2:
+	case MDP_Y_CBCR_H1V2:
 	case MDP_Y_CRCB_H2V2:
 	case MDP_Y_CBCR_H2V2:
 	case MDP_Y_CRCB_H1V1:
@@ -1290,6 +1296,14 @@ int mdp4_overlay_format2pipe(struct mdp4_overlay_pipe *pipe)
 				pipe->chroma_sample = MDP4_CHROMA_H1V2;
 			else
 				pipe->chroma_sample = MDP4_CHROMA_RGB;
+		} else if (pipe->src_format == MDP_Y_CRCB_H1V2) {
+			pipe->element1 = C1_B_Cb;
+			pipe->element0 = C2_R_Cr;
+			pipe->chroma_sample = MDP4_CHROMA_H1V2;
+		} else if (pipe->src_format == MDP_Y_CBCR_H1V2) {
+			pipe->element1 = C2_R_Cr;
+			pipe->element0 = C1_B_Cb;
+			pipe->chroma_sample = MDP4_CHROMA_H1V2;
 		} else if (pipe->src_format == MDP_Y_CRCB_H2V2) {
 			pipe->element1 = C1_B_Cb;
 			pipe->element0 = C2_R_Cr;
@@ -1450,6 +1464,8 @@ void transp_color_key(int format, uint32 transp,
 	case MDP_Y_CR_CB_GH2V2:
 	case MDP_Y_CRCB_H2V2:
 	case MDP_Y_CRCB_H2V1:
+	case MDP_Y_CRCB_H1V2:
+	case MDP_Y_CBCR_H1V2:
 	case MDP_Y_CRCB_H1V1:
 	case MDP_Y_CBCR_H1V1:
 	case MDP_YCRCB_H1V1:
@@ -3635,7 +3651,9 @@ int mdp4_overlay_play(struct fb_info *info, struct msmfb_overlay_data *req)
 		}
 		pipe->srcp0_ystride = pipe->src_width;
 		if ((pipe->src_format == MDP_Y_CRCB_H1V1) ||
-			(pipe->src_format == MDP_Y_CBCR_H1V1)) {
+			(pipe->src_format == MDP_Y_CBCR_H1V1) ||
+			(pipe->src_format == MDP_Y_CRCB_H1V2) ||
+			(pipe->src_format == MDP_Y_CBCR_H1V2)) {
 			if (pipe->src_width > YUV_444_MAX_WIDTH)
 				pipe->srcp1_ystride = pipe->src_width << 2;
 			else
