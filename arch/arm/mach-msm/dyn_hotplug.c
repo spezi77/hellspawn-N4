3ef1a1f26b22b4e3e26b2f83d25053607476aeb3
Stratos Karafotis <stratosk@semaphore.gr>
2013-07-22 00:06:37 +0300

    mach-msm: dyn_hotplug: Add module parameters to control driver
    Add module parameters for
    - up_threshold
    - enabled
    - min_online
    
    Signed-off-by: Stratos Karafotis <stratosk@semaphore.gr>

--- arch/arm/mach-msm/dyn_hotplug.c
+++ arch/arm/mach-msm/dyn_hotplug.c
@@ -28,15 +28,29 @@
 #define UP_THRESHOLD	(25)
 #define MIN_ONLINE	(2)
 
+static int enabled;
+static unsigned int up_threshold;
+static unsigned int min_online;
+
 struct dyn_hp_data {
 	unsigned int up_threshold;
 	unsigned int delay;
 	unsigned int min_online;
 	unsigned int down_timer;
 	unsigned int up_timer;
+	unsigned int enabled;
 	struct delayed_work work;
 	struct early_suspend suspend;
 } *hp_data;
+
+static inline void up_all(void)
+{
+	unsigned int cpu;
+
+	for_each_possible_cpu(cpu)
+		if (!cpu_online(cpu))
+			cpu_up(cpu);
+}
 
 static void hp_early_suspend(struct early_suspend *h)
 {
@@ -46,13 +60,9 @@ static void hp_early_suspend(struct early_suspend *h)
 
 static __cpuinit void hp_late_resume(struct early_suspend *h)
 {
-	unsigned int cpu;
-
 	pr_debug("%s: num_online_cpus: %u\n", __func__, num_online_cpus());
-	for_each_possible_cpu(cpu)
-		if (!cpu_online(cpu))
-			cpu_up(cpu);
 
+	up_all();
 	hp_data->down_timer = 0;
 }
 
@@ -111,6 +121,79 @@ static __cpuinit void load_timer(struct work_struct *work)
 	schedule_delayed_work(&hp_data->work, hp_data->delay);
 }
 
+static void dyn_hp_enable(void)
+{
+	schedule_delayed_work(&hp_data->work, hp_data->delay);
+	hp_data->enabled = 1;
+	enabled = hp_data->enabled;
+}
+
+static void dyn_hp_disable(void)
+{
+	cancel_delayed_work(&hp_data->work);
+	flush_scheduled_work();
+	up_all();
+	hp_data->enabled = 0;
+	enabled = hp_data->enabled;
+}
+
+static __cpuinit int set_enabled(const char *val, const struct kernel_param *kp)
+{
+	int ret = 0;
+
+	ret = param_set_bool(val, kp);
+	hp_data->enabled = enabled;
+	if (!hp_data->enabled)
+		dyn_hp_disable();
+	else
+		dyn_hp_enable();
+
+	pr_info("%s: enabled = %d\n", __func__, hp_data->enabled);
+	return ret;
+}
+
+static struct kernel_param_ops enabled_ops = {
+	.set = set_enabled,
+	.get = param_get_bool,
+};
+
+module_param_cb(enabled, &enabled_ops, &enabled, 0644);
+MODULE_PARM_DESC(enabled, "control dyn_hotplug");
+
+static int set_up_threshold(const char *val, const struct kernel_param *kp)
+{
+	int ret = 0;
+
+	ret = param_set_uint(val, kp);
+	hp_data->up_threshold = up_threshold;
+	return ret;
+}
+
+static struct kernel_param_ops up_threshold_ops = {
+	.set = set_up_threshold,
+	.get = param_get_uint,
+};
+
+module_param_cb(up_threshold, &up_threshold_ops, &up_threshold, 0644);
+
+static __cpuinit int set_min_online(const char *val,
+						const struct kernel_param *kp)
+{
+	int ret = 0;
+
+	ret = param_set_uint(val, kp);
+	hp_data->min_online = min_online;
+	up_all();
+	return ret;
+}
+
+static struct kernel_param_ops min_online_ops = {
+	.set = set_min_online,
+	.get = param_get_uint,
+};
+
+module_param_cb(min_online, &min_online_ops, &min_online, 0644);
+
 static int __init dyn_hp_init(void)
 {
 	hp_data = kzalloc(sizeof(*hp_data), GFP_KERNEL);
@@ -124,6 +207,10 @@ static int __init dyn_hp_init(void)
 	hp_data->suspend.suspend = hp_early_suspend;
 	hp_data->suspend.resume =  hp_late_resume;
 	hp_data->suspend.level = EARLY_SUSPEND_LEVEL_BLANK_SCREEN;
+	hp_data->enabled = 1;
+	up_threshold = hp_data->up_threshold;
+	enabled = hp_data->enabled;
+	min_online = hp_data->min_online;
 	register_early_suspend(&hp_data->suspend);
 
 	INIT_DELAYED_WORK(&hp_data->work, load_timer);
