cbb76dc1fb4a7424200d240a15ef33c9aee24af6
mrg666 <drgungor@hotmail.com>
2014-03-05 21:12:15 -0500

    [LINARO] workqueues: Introduce new flag WQ_POWER_EFFICIENT for power oriented workqueues
    Adapted for 3.4 from lsk-v3.10
    
    Workqueues can be performance or power-oriented. Currently, most workqueues are
    bound to the CPU they were created on. This gives good performance (due to cache
    effects) at the cost of potentially waking up otherwise idle cores (Idle from
    scheduler's perspective. Which may or may not be physically idle) just to
    process some work. To save power, we can allow the work to be rescheduled on a
    core that is already awake.
    
    Workqueues created with the WQ_UNBOUND flag will allow some power savings.
    However, we don't change the default behaviour of the system.  To enable
    power-saving behaviour, a new config option CONFIG_WQ_POWER_EFFICIENT needs to
    be turned on. This option can also be overridden by the
    workqueue.power_efficient boot parameter.
    
    tj: Updated config description and comments.  Renamed
        CONFIG_WQ_POWER_EFFICIENT to CONFIG_WQ_POWER_EFFICIENT_DEFAULT.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Reviewed-by: Amit Kucheria <amit.kucheria@linaro.org>
    Signed-off-by: Tejun Heo <tj@kernel.org>
    (cherry picked from commit cee22a1)
    Signed-off-by: Mark Brown <broonie@linaro.org>
    Signed-off-by: Luis Cruz <ljc2491@gmail.com>
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -43,6 +43,8 @@
 #include <linux/idr.h>
 #include <linux/bug.h>
 #include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/hashtable.h>
 
 #include "workqueue_sched.h"
 
@@ -3299,6 +3301,10 @@ struct workqueue_struct *__alloc_workqueue_key(const char *fmt,
 	struct workqueue_struct *wq;
 	unsigned int cpu;
 	size_t namelen;
+
+	/* see the comment above the definition of WQ_POWER_EFFICIENT */
+	if ((flags & WQ_POWER_EFFICIENT) && wq_power_efficient)
+		flags |= WQ_UNBOUND;
 
 	/* determine namelen, allocate wq and format name */
 	va_start(args, lock_name);
