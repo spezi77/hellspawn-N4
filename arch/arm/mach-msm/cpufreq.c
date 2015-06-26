3106d7d32d490d8fa9a95cb67a20244486e951e9
myfluxi <linflux@arcor.de>
2015-06-26 15:55:05 +0200

    msm: cpufreq: Break early if target freq is current freq
    Change-Id: Id8a993969ed47ab5d1e7eeaf7941ba044757da62
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -140,8 +140,10 @@ static int msm_cpufreq_target(struct cpufreq_policy *policy,
 
 	mutex_lock(&per_cpu(cpufreq_suspend, policy->cpu).suspend_mutex);
 
-	if (target_freq == policy->cur)
+	if (target_freq == policy->cur) {
+		ret = 0;
 		goto done;
+	}
 
 	if (per_cpu(cpufreq_suspend, policy->cpu).device_suspended) {
 		pr_debug("cpufreq: cpu%d scheduling frequency change "
