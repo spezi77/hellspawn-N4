2452d3182bf04e1ce8ead97a2e0d29abb3d41dab
brandonabandon <supertuesday666@gmail.com>
2015-12-14 16:40:18 -0500

    gcc5 compiling
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- scripts/gcc-wrapper.py
+++ scripts/gcc-wrapper.py
@@ -43,6 +43,13 @@ allowed_warnings = set([
     "alignment.c:327",
     "mmu.c:602",
     "return_address.c:62",
+    "mipi_lgit.c:600",
+    "peripheral-loader.c:579",
+    "vcd_res_tracker.c:605",
+    "wlan_hdd_wmm.c:847",
+    "wcd9304.c:3699",
+    "wcd9310.c:6051",
+    "schBeaconGen.c:96"
  ])
 
 # Capture the name of the object file, can find it.
