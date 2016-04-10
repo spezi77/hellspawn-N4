7ffef35e593f6057a06cd874184ff4db15cd2dc0
spezi77 <spezi77@gmx.com>
2016-04-10 14:01:55 +0200

    cpufreq: add slim governor (experimental). Based a lot on elementalX by flar2. Has been further developed by kufikugel (SlimROMs).
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -386,6 +386,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HELLSACTIVE)
 extern struct cpufreq_governor cpufreq_gov_hellsactive;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_hellsactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SLIM)
+extern struct cpufreq_governor cpufreq_gov_slim;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_slim)
 #endif
 
 
