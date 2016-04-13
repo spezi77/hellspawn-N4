0ce14dbfb64f316e9e18a152e257d629e24f3679
spezi77 <spezi77@gmx.com>
2016-04-13 20:41:51 +0200

    cpufreq: adios slim governor. elementalX performes much better!
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -386,9 +386,6 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HELLSACTIVE)
 extern struct cpufreq_governor cpufreq_gov_hellsactive;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_hellsactive)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SLIM)
-extern struct cpufreq_governor cpufreq_gov_slim;
-#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_slim)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BADASS)
 extern struct cpufreq_governor cpufreq_gov_badass;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_badass)
