23cc778ef50a0569b6619d7376b61372cd1f6138
spezi77 <spezi77@gmx.com>
2016-04-23 01:08:56 +0200

    Enable Alucard Hotplug & introduce Alucard CPU governor
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -392,6 +392,9 @@ extern struct cpufreq_governor cpufreq_gov_hellsactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PEGASUSQ)
 extern struct cpufreq_governor cpufreq_gov_pegasusq;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_pegasusq)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ALUCARD)
+extern struct cpufreq_governor cpufreq_gov_alucard;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_alucard)
 #endif
 
 /*********************************************************************
