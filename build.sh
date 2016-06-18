72f967ae90bbe96e92eb13bdf9106072e82d8e66
spezi77 <spezi77@gmx.com>
2016-06-18 09:38:45 +0200

    Add my build script and update defconfig for N.
    Signed-off-by: spezi77 <spezi77@gmx.com>

---
+++ build.sh
@@ -0,0 +1,157 @@
+#!/bin/bash
+
+# Bash Color
+green='\033[01;32m'
+red='\033[01;31m'
+blink_red='\033[05;31m'
+restore='\033[0m'
+
+clear
+
+# Resources
+THREAD="-j6"
+KERNEL="zImage"
+DEFCONFIG="mako_defconfig"
+
+# Kernel Details
+BASE_HC_VER="N4-nougat-7.0-stock"
+VER="-r01"
+HC_VER="$BASE_HC_VER$VER"
+
+# Vars
+export ARCH=arm
+export SUBARCH=arm
+export CROSS_COMPILE=/home/spezi77/android/uber-tc/arm-eabi-5.x/bin/arm-eabi-
+export LOCALVERSION="$HC_VER"
+
+# Paths
+KERNEL_DIR=`pwd`
+REPACK_DIR="${HOME}/android/Kernel/anykernel_msm"
+ZIP_MOVE="${HOME}/android/Kernel/Releases/N4-nougat-7.0"
+ZIMAGE_DIR="${HOME}/android/purenexus-n/kernel/lge/mako/arch/arm/boot"
+DB_FOLDER="${HOME}/Dropbox/Kernel-Betas/N4-nougat-7.0"
+
+# Functions
+function clean_all {
+		rm -rf $REPACK_DIR/tmp/anykernel/zImage
+		make clean && make mrproper
+}
+
+function make_kernel {
+		make $DEFCONFIG
+		make $THREAD
+		cp -vr $ZIMAGE_DIR/$KERNEL $REPACK_DIR/tmp/anykernel
+}
+
+function make_zip {
+		cd $REPACK_DIR
+		zip -9 -r --exclude='*.git*' `echo $HC_VER`.zip .
+		mv  `echo $HC_VER`.zip $ZIP_MOVE
+		cd $KERNEL_DIR
+}
+
+function copy_dropbox {
+		cd $ZIP_MOVE
+		cp -vr  `echo $HC_VER`.zip $DB_FOLDER
+		cd $KERNEL_DIR
+}
+
+DATE_START=$(date +"%s")
+
+echo -e "${green}"
+echo "N4 Nougat Kernel Creation Script:"
+echo
+
+echo "---------------"
+echo "Kernel Version:"
+echo "---------------"
+
+echo -e "${red}"; echo -e "${blink_red}"; echo "$HC_VER"; echo -e "${restore}";
+
+echo -e "${green}"
+echo "-----------------"
+echo "Making Kernel:"
+echo "-----------------"
+echo -e "${restore}"
+
+echo "----------------------------"
+echo "Please choose your option:"
+echo "----------------------------"
+while read -p " [1]clean-build / [2]dirty-build / [3]abort " cchoice
+do
+case "$cchoice" in
+	1 )
+		HC_VER="$BASE_HC_VER$VER"
+		echo -e "${green}"
+		echo
+		echo "[..........Cleaning up..........]"
+		echo
+		echo -e "${restore}"
+		clean_all
+		echo -e "${green}"
+		echo
+		echo "[....Building `echo $HC_VER`....]"
+		echo
+		echo -e "${restore}"
+		make_kernel
+		echo -e "${green}"
+		echo
+		echo "[....Make `echo $HC_VER`.zip....]"
+		echo
+		echo -e "${restore}"
+		make_zip
+		echo -e "${green}"
+		echo
+		echo "[.....Moving `echo $HC_VER`.....]"
+		echo
+		echo -e "${restore}"
+		copy_dropbox
+		break
+		;;
+	2 )
+		HC_VER="$BASE_HC_VER$VER"
+		echo -e "${green}"
+		echo
+		echo "[....Building `echo $HC_VER`....]"
+		echo
+		echo -e "${restore}"
+		make_kernel
+		echo -e "${green}"
+		echo
+		echo "[....Make `echo $HC_VER`.zip....]"
+		echo
+		echo -e "${restore}"
+		make_zip
+		echo -e "${green}"
+		echo
+		echo "[.....Moving `echo $HC_VER`.....]"
+		echo
+		echo -e "${restore}"
+		copy_dropbox
+		break
+		;;
+	3 )
+		break
+		;;
+	* )
+		echo -e "${red}"
+		echo
+		echo "Invalid try again!"
+		echo
+		echo -e "${restore}"
+		;;
+esac
+done
+
+echo -e "${green}"
+echo "-------------------"
+echo "Build Completed in:"
+echo "-------------------"
+echo -e "${restore}"
+
+DATE_END=$(date +"%s")
+DIFF=$(($DATE_END - $DATE_START))
+echo "Time: $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
+echo
+
+
